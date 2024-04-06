cee432bedf6205fcdb60a4ce6b2dd9fb23c25f78
szvitek <gy.szvitek@gmail.com>
2024-04-06 17:56:22 +0100

    feat(ItemList): UseState, Map, Input, Checkboxex

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,3 +1,26 @@
+import { useState } from 'react';
+import { initialItems } from '../lib/constants';
+
 export default function ItemList() {
-  return <div>ItemList</div>;
+  const [items] = useState(initialItems);
+
+  return (
+    <ul>
+      {items.map((item) => (
+        <Item key={item.id} item={item} />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li className="item">
+      <label>
+        <input checked={item.packed} type="checkbox" />
+        {item.name}
+      </label>
+      <button>❌</button>
+    </li>
+  );
 }
