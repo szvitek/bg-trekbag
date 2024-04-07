95109789751c5171f8f9d36d23f7af9850dfbb86
szvitek <gy.szvitek@gmail.com>
2024-04-07 11:58:03 +0100

    feat(ItemList): sorting items, react-select

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,47 @@
+import Select from 'react-select';
 import EmptyView from './EmptyView';
+import { useState } from 'react';
+
+const sortingOptions = [
+  { value: 'default', label: 'Sort by default' },
+  { value: 'packed', label: 'Sort by packed' },
+  { value: 'unpacked', label: 'Sort by unpacked' },
+];
 
 export default function ItemList({
   items,
   handleDeleteItem,
   handleToggleItem,
 }) {
+  const [sortBy, setSortBy] = useState('default');
+  // or .toSorted() instead of spreading
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === 'packed') {
+      return b.packed - a.packed;
+    }
+
+    if (sortBy === 'unpacked') {
+      return a.packed - b.packed;
+    }
+
+    return;
+  });
+
   return (
     <ul className="item-list">
       {items.length === 0 ? <EmptyView /> : null}
-      {items.map((item) => (
+
+      {items.length > 0 ? (
+        <section className="sorting">
+          <Select
+            options={sortingOptions}
+            defaultValue={sortingOptions[0]}
+            onChange={(option) => setSortBy(option.value)}
+          />
+        </section>
+      ) : null}
+
+      {sortedItems.map((item) => (
         <Item
           key={item.id}
           item={item}
