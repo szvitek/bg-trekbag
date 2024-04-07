ff38f8648a5e50db293046430ce6c9a6ac792c20
szvitek <gy.szvitek@gmail.com>
2024-04-07 01:05:05 +0100

    feat(ItemList): Delete and toggle individual item

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,22 @@ function App() {
     setItems(newItems);
   };
 
+  const handleDeleteItem = (id) => {
+    const newItems = items.filter((item) => item.id !== id);
+    setItems(newItems);
+  };
+
+  const handleToggleItem = (id) => {
+    const newItems = items.map((item) => {
+      if (item.id === id) {
+        return { ...item, packed: !item.packed };
+      }
+
+      return item;
+    });
+    setItems(newItems);
+  };
+
   const handleRemoveAllItems = () => {
     setItems([]);
   };
@@ -28,10 +44,12 @@ function App() {
   };
 
   const handleMarkAllAsComplete = () => {
-    setItems(items.map(item => ({
-      ...item,
-      packed: true
-    })));
+    setItems(
+      items.map((item) => ({
+        ...item,
+        packed: true,
+      }))
+    );
   };
 
   const handleMarkAllAsIncomplete = () => {
@@ -48,7 +66,11 @@ function App() {
       <BackgroundHeading />
       <main>
         <Header />
-        <ItemList items={items} />
+        <ItemList
+          items={items}
+          handleDeleteItem={handleDeleteItem}
+          handleToggleItem={handleToggleItem}
+        />
         <Sidebar
           handleAddItem={handleAddItem}
           handleRemoveAllItems={handleRemoveAllItems}
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,21 +1,30 @@
-export default function ItemList({ items }) {
+export default function ItemList({ items, handleDeleteItem, handleToggleItem }) {
   return (
     <ul>
       {items.map((item) => (
-        <Item key={item.id} item={item} />
+        <Item
+          key={item.id}
+          item={item}
+          handleDeleteItem={handleDeleteItem}
+          handleToggleItem={handleToggleItem}
+        />
       ))}
     </ul>
   );
 }
 
-function Item({ item }) {
+function Item({ item, handleDeleteItem, handleToggleItem }) {
   return (
     <li className="item">
       <label>
-        <input checked={item.packed} type="checkbox" />
+        <input
+          checked={item.packed}
+          type="checkbox"
+          onChange={() => handleToggleItem(item.id)}
+        />
         {item.name}
       </label>
-      <button>❌</button>
+      <button onClick={() => handleDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
